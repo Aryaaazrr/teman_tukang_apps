38b079ce9e7af019a2a42cac77296d3c98c94e88
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2024-11-28 02:59:57 +0700

    update tailwind config custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,45 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./node_modules/flowbite/**/*.js",
     ],
-
+    darkMode: "class",
     theme: {
         extend: {
+            keyframes: {
+                customBounce: {
+                    "0%, 100%": { transform: "translateY(0)" },
+                    "50%": { transform: "translateY(-10px)" },
+                },
+            },
+            animation: {
+                customBounce: "customBounce 2s ease-in-out infinite",
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: {
+                    50: "#fefce8",
+                    100: "#fef9c3",
+                    200: "#fef08a",
+                    300: "#fde047",
+                    400: "#facc15",
+                    500: "#eab308",
+                    600: "#ca8a04",
+                    700: "#a16207",
+                    800: "#854d0e",
+                    900: "#422006",
+                },
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, require("flowbite/plugin")],
 };
