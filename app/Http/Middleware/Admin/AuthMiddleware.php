e375cadec247263b48b650081425254340180791
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2024-11-29 03:32:13 +0700

    update auth middleware admin

--- app/Http/Middleware/Admin/AuthMiddleware.php
+++ app/Http/Middleware/Admin/AuthMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware\Admin;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthMiddleware
@@ -15,7 +16,7 @@ class AuthMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!auth()->check()) {
+        if (!Auth::check()) {
             return redirect()->route('admin.auth.login');
         }
         return $next($request);
