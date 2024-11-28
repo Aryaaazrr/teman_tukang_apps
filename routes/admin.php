85758737ae6cfd9f275776c48a01e424a8b7edcc
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2024-11-29 02:41:19 +0700

    adding route admin prefix

---
+++ routes/admin.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Admin\Auth\LogoutController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin-panel')
+    ->name('admin.')
+    ->namespace('App\Http\Controllers\Admin')
+    // ->middleware(['admin'])
+    ->group(function () {
+
+    Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
+        Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
+        Route::post('/login', [LoginController::class, 'store'])->name('auth.login.process');
+        Route::get('/logout', [LogoutController::class, 'index'])->name('auth.logout');
+    });
+});
