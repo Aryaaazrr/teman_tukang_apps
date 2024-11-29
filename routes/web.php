c857375d25404d13191c90c2d4dad9894c5f3944
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2024-11-30 03:09:10 +0700

    update route web company

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Website\About\AboutController;
+use App\Http\Controllers\Website\Contact\ContactController;
+use App\Http\Controllers\Website\FAQ\FAQController;
+use App\Http\Controllers\Website\Features\FeaturesController;
 use App\Http\Controllers\Website\Home\HomeController;
+use App\Http\Controllers\Website\Services\ServicesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('about', [AboutController::class, 'index'])->name('about.index');
+
+Route::get('features', [FeaturesController::class, 'index'])->name('features.index');
+
+Route::get('services', [ServicesController::class, 'index'])->name('services.index');
+
+Route::get('faq', [FAQController::class, 'index'])->name('faq.index');
+
+Route::get('contact', [ContactController::class, 'index'])->name('contact.index');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
