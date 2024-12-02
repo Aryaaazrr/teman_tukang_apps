15e92dbb3a037c85b808a2a0dfa4731b625ea008
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2024-12-02 23:58:20 +0700

    delete mthod ga kepake

--- routes/auth.php
+++ routes/auth.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\ThirdParty\Google\GoogleLoginController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function () {
